e5be6356db203c14b6c283ed1ff0d749b84e8c99
Bryant <Bryant.allsup@gmail.com>
2025-01-06 18:21:12 -0700

    added Mistyped, loops and more depth to the sleep function. Right now the mistyped functionality is not random and will just give a set word depending on the placement of word selection(the word in x postion in the sentence, will get the x incorrect option in the mistyped options)charSelection will just write the mistyped option selected. From here I need to add a randomization factor in deciding the incoorect/correct option when typing a word as well as adding a way to correct the incorrect typing.

--- main.js
+++ main.js
@@ -1,15 +1,69 @@
-const {Builder,By,Key,util, promise} = require("selenium-webdriver");
+import {create,all} from 'mathjs';
+import {Builder,By,Key} from "selenium-webdriver";
+import Mistyped from 'mistyped';
 
+const math = create(all);
+const options = 
+{
+    startAfter: 0,
+    dimension: 0,
+
+}
 
 async function search(){
     let driver = await new Builder().forBrowser("MicrosoftEdge").build();
     await driver.get("https://www.google.com");
-    await driver.findElement(By.name("q")).sendKeys("What are online cookies?");
-    await sleep(10);
+
+    let sentence = "what are online cookies?";
+    let sentenceSplit = sentence.split(" ");
+    console.log(sentenceSplit);
+    
+
+    for (let wordSelection = 0; wordSelection<sentenceSplit.length;wordSelection++)
+    {
+        
+        
+        let mistypedInstance = Mistyped(sentenceSplit[wordSelection]);//right now I will have mistyped word 0 "what" with mistake 0 "wgat",same with each following word
+        
+        if(wordSelection<sentenceSplit.length)
+            mistypedInstance[wordSelection] = mistypedInstance[wordSelection]+' ';
+        
+        for(let charSelection =0; charSelection<mistypedInstance.length;charSelection++)
+        {
+            try{
+                await driver.findElement(By.name("q")).sendKeys(mistypedInstance[wordSelection].charAt(charSelection));
+            }
+            catch(e){
+                console.log(e instanceof TypeError);
+                console.log(mistypedInstance[wordSelection].charAt(charSelection));
+            }
+            await sleep(.2,.4);//.4,.8
+        }
+    }
     await driver.findElement(By.name("q")).sendKeys(Key.ENTER);
+
+    
+
 }
 
-async function sleep(seconds) {
-    return new Promise(resolve=>(setTimeout(resolve,1000*seconds)));
+async function sleep(min,max) {
+        let pause = math.random();
+        if (pause>.00001)
+        {
+            let delay = (min+(max-min)*pause)*1000;
+            return new Promise(resolve=>(setTimeout(resolve,delay)));
+        }
+        else
+        {
+            let distraction_min=1;
+            let distraction_max=4;
+            let disrtaction_delay = (distraction_min+(distraction_max-distraction_min)*pause)*1000;
+            return new Promise(resolve=>(setTimeout(resolve,disrtaction_delay)));
+        }
 }
+async function checkPoints()
+{
+    await driver.get("https://rewards.bing.com/");
+}
+
 search();
