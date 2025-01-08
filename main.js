a7260ee43b2624f268f485eee59a70a368477144
Bryant <Bryant.allsup@gmail.com>
2025-01-07 17:14:48 -0700

    try catch involving anything to do with the driver. Added variable instead of hard values for typingacc, typing speed and page delay, page will now close when done with search, got spacing to work when typing, added more realistic typing than just a random number generator and will now be able to adjust how likley to get wrong in the chosenWord function and inmprove way that the wrong word is selected using the new randomNumberPortions function.

--- main.js
+++ main.js
@@ -1,69 +1,92 @@
-import {create,all} from 'mathjs';
-import {Builder,By,Key} from "selenium-webdriver";
+import {create,all, random} from 'mathjs';
+import {Builder,By,Key, promise} from "selenium-webdriver";
 import Mistyped from 'mistyped';
 
 const math = create(all);
 const options = 
 {
     startAfter: 0,
-    dimension: 0,
+    dimension: 2,
     includeMisplaced: true,
 }
+let typingAccuracy = .75;
+let sentence = "what are online cookies?";
+let typingSpeed=[.2,.4];
+let pageDelay=[5,20];
 
-async function search(){
-    let driver = await new Builder().forBrowser("MicrosoftEdge").build();
-    await driver.get("https://www.google.com");
 
-    let sentence = "what are online cookies?";
-    let sentenceSplit = sentence.split(" ");
-    console.log(sentenceSplit);
-    
+async function search(typingAccuracy,sentence,typingSpeed,pageDelay){
+    let driver;
+    try
+        {
+        driver = await new Builder().forBrowser("MicrosoftEdge").build();
+        await driver.get("https://www.google.com");
 
+        let sentenceSplit = sentence.split(" ");
+        isCorrect = [sentenceSplit.length*true];
     for (let wordSelection = 0; wordSelection<sentenceSplit.length;wordSelection++)
     {
+        let typedWord;
+        let mistypedInstance = Mistyped(sentenceSplit[wordSelection],options);
+        if(wordSelection<sentenceSplit.length-1)
+        {typedWord = await chosenWord(mistypedInstance,sentenceSplit[wordSelection],typingAccuracy)+' ';}
+        else
+        {typedWord = await chosenWord(mistypedInstance,sentenceSplit[wordSelection],typingAccuracy);}
         
-        
-        let mistypedInstance = Mistyped(sentenceSplit[wordSelection],options);//right now I will have mistyped word 0 "what" with mistake 0 "wgat",same with each following word
-        
-        if(wordSelection<sentenceSplit.length)
-            mistypedInstance[wordSelection] = mistypedInstance[wordSelection]+' ';
-        
-        for(let charSelection =0; charSelection<mistypedInstance.length;charSelection++)
+        for(let charSelection =0; charSelection<typedWord.length;charSelection++)
         {
             try{
-                await driver.findElement(By.name("q")).sendKeys(mistypedInstance[wordSelection].charAt(charSelection));
+                await driver.findElement(By.name("q")).sendKeys(typedWord.charAt(charSelection));
             }
             catch(e){
-                console.log(e instanceof TypeError);
-                console.log(mistypedInstance[wordSelection].charAt(charSelection));
+                console.error("Error during typing: ", e.message);
             }
-            await sleep(.2,.4);//.4,.8
+            await sleep(typingSpeed[0],typingSpeed[1]);
         }
     }
     await driver.findElement(By.name("q")).sendKeys(Key.ENTER);
-
-    
-
+    await sleep(pageDelay[0],pageDelay[1]);
+} 
+    catch(e)
+        {console.error("Driver Iniziatization Failed",e.message);}
+    finally 
+        {if (driver) await driver.quit();}
 }
 
 async function sleep(min,max) {
         let pause = math.random();
-        if (pause>.00001)
+        if (pause>.01)
         {
             let delay = (min+(max-min)*pause)*1000;
             return new Promise(resolve=>(setTimeout(resolve,delay)));
         }
         else
         {
-            let distraction_min=1;
-            let distraction_max=4;
+            let distraction_min=3;
+            let distraction_max=7;
             let disrtaction_delay = (distraction_min+(distraction_max-distraction_min)*pause)*1000;
             return new Promise(resolve=>(setTimeout(resolve,disrtaction_delay)));
         }
 }
-async function checkPoints()
+
+//Randomly gets a number between 0 and 99, then if number is available will use the incorrect choice or the correct choice if none is. 
+async function chosenWord(incorrectChoices,originalWord,chanceOfSuccess)
 {
-    await driver.get("https://rewards.bing.com/");
+    let randomError = math.random();
+
+    if(randomError>chanceOfSuccess)//determines if an error occured
+        {   
+            let randomNumber = randomNumberPortions(incorrectChoices.length);
+            return incorrectChoices[randomNumber];
+        }
+    else
+        {return originalWord;}
+}
+/*take the total number of choices and returns */ 
+function randomNumberPortions(total)
+{
+    return total === 0?0:math.floor(math.random()*total);
 }
 
-search();
+
+search(typingAccuracy,sentence,typingSpeed,pageDelay);
