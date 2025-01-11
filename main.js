fbb540f4ce0b3a310b5e92959aa5c08782d4b93c
Bryant <Bryant.allsup@gmail.com>
2025-01-10 17:40:20 -0700

    created a new json file and am planning on adding the functionality in order to have a catalog of sentences. Fi bug involving the incorrect deletion and adding of characters in the sentences.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {create,all, random} from 'mathjs';
+import {create,all, random, e} from 'mathjs';
 import {Builder,By,Key, promise} from "selenium-webdriver";
 import Mistyped from 'mistyped';
 
@@ -9,10 +9,11 @@ const options =
     dimension: 2,
     includeMisplaced: true,
 }
-let typingAccuracy = .9;
-let sentence = "What am I doing with my life?";
-let typingSpeed=[.2,.4];
-let pageDelay=[5,20];
+let typingAccuracy = .95;
+let sentence = "";
+let typingSpeed=[.3,.4];
+let pageDelay=[10,20];
+let log ="";
 
 
 async function search(typingAccuracy,sentence,typingSpeed,pageDelay){
@@ -20,30 +21,41 @@ async function search(typingAccuracy,sentence,typingSpeed,pageDelay){
     try
     {
         driver = await new Builder().forBrowser("MicrosoftEdge").build();
-        await driver.get("https://www.google.com");
-
+        await driver.get("https://www.duckduckgo.com");
         let sentenceSplit = sentence.split(" ");
-
         for (let wordSelection = 0; wordSelection<sentenceSplit.length;wordSelection++)
         {
-            let typedWord;
-            let mistypedInstance = Mistyped(sentenceSplit[wordSelection],options);
-            if(wordSelection<sentenceSplit.length-1)
-            {typedWord = chosenWord(mistypedInstance,sentenceSplit[wordSelection],typingAccuracy)+' ';}
-            else
-            {typedWord = chosenWord(mistypedInstance,sentenceSplit[wordSelection],typingAccuracy);}
-            console.log(IncorrectWord(typedWord,sentenceSplit[wordSelection]));
-            for(let charSelection =0; charSelection<typedWord.length;charSelection++)
+            let actualWord = sentenceSplit[wordSelection];
+            for(let charSelection =0; charSelection<actualWord.length;charSelection++)
             {
+                let mistypedChar = Mistyped(sentenceSplit[wordSelection].charAt(charSelection),options);// gives a list of possible incorrect chars that can be chosen.
+                
+                let typedChar = chosenChar(mistypedChar,actualWord.charAt(charSelection),typingAccuracy);// determines wheter char is right or wrong.
                 try
-                    {await driver.findElement(By.name("q")).sendKeys(typedWord.charAt(charSelection));}
+                    {await driver.findElement(By.name("q")).sendKeys(typedChar);log+=typedChar;}
                 catch(e)
                     {console.error("Error during typing: ", e.message);}
-            await sleep(typingSpeed[0],typingSpeed[1]);
+                await sleep(typingSpeed[0],typingSpeed[1]);
+                
+                try
+                {
+                if (!checkChar(typedChar,actualWord.charAt(charSelection)))
+                    {   
+                        await driver.findElement(By.name("q")).sendKeys(Key.BACK_SPACE);log+='D';
+                        await sleep(typingSpeed[0],typingSpeed[1]);
+                        charSelection=charSelection-1;
+                    }
+                }
+                catch(e)
+                    {console.error("Error during typing: ", e.message);}
+
             }
+            await driver.findElement(By.name("q")).sendKeys(Key.SPACE);log+=' ';
+            
         }
         await driver.findElement(By.name("q")).sendKeys(Key.ENTER);
         await sleep(pageDelay[0],pageDelay[1]);
+        console.log(log);
     } 
     catch(e)
         {console.error("Driver Iniziatization Failed",e.message);}
@@ -53,7 +65,7 @@ async function search(typingAccuracy,sentence,typingSpeed,pageDelay){
 
 async function sleep(min,max) {
         let pause = math.random();
-        if (pause>.01)
+        if (pause>.001)
         {
             let delay = (min+(max-min)*pause)*1000;
             return new Promise(resolve=>(setTimeout(resolve,delay)));
@@ -68,23 +80,34 @@ async function sleep(min,max) {
 }
 
 //Randomly gets a number between 0 and 99, then if number is available will use the incorrect choice or the correct choice if none is. 
-function chosenWord(incorrectChoices,originalWord,chanceOfSuccess){
+function chosenChar(incorrectChoices,originalChar,chanceOfSuccess){
     let randomError = math.random();
-
     if(randomError>chanceOfSuccess)//determines if an error occured
         {   
             let randomNumber = randomNumberPortions(incorrectChoices.length);
-            return incorrectChoices[randomNumber];
+            console.log(randomNumber);
+            if(incorrectChoices[randomNumber]){//determines if an error is valid
+                return incorrectChoices[randomNumber];
+            }
+            return originalChar;
         }
     else
-        {return originalWord;}
+        {
+            return originalChar;
+        }
 }
 
 function randomNumberPortions(total)
     {return total === 0?0:math.floor(math.random()*total);}
 
-function IncorrectWord(typedWord,actualWord)
-{return typedWord===actualWord?true:false;}
+function checkChar(typedChar,actualChar)
+    {return typedChar===actualChar}
 
+function main()
+{
+    fetch("Sentences.json");
+    console.log("Fetched!");
+    //search(typingAccuracy,sentence,typingSpeed,pageDelay);
+}
 
-search(typingAccuracy,sentence,typingSpeed,pageDelay);
+main();
